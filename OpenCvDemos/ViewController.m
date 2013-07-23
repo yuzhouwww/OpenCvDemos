7bb0ee33925227f574b16fdafa81087ca1a1c847
yuzhouwww <469258988@qq.com>
2013-07-23 22:33:29 +0800

    Initial Commit

---
+++ OpenCvDemos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OpenCvDemos
+//
+//  Created by yuzhou on 13-7-23.
+//  Copyright (c) 2013年 wzyk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
