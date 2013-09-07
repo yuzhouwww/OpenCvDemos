73c009d279f9b3ffdfbe14a11eb3b59b0b8255e9
yuzhouwww <469258988@qq.com>
2013-09-08 01:23:11 +0800

    ...

--- OpenCvDemos/ViewController.mm
+++ OpenCvDemos/ViewController.mm
@@ -84,6 +84,7 @@ void detectWithFlann(Mat img0, int width0, int height0, Mat img, int width, int 
     FlannBasedMatcher matcher(indexParams, searchParams);
     OrbFeatureDetector detector(1000);
     OrbDescriptorExtractor extractor(1000);
+    
     vector<KeyPoint> keyPoints0;
     Mat descriptor0;
     detector.detect(img0, keyPoints0, descriptor0);
@@ -92,18 +93,21 @@ void detectWithFlann(Mat img0, int width0, int height0, Mat img, int width, int 
     vector<Mat> descriptors;
     descriptors.push_back(descriptor0);
     matcher.add(descriptors);
+    
+    NSDate *date2 = [NSDate date];
     vector<KeyPoint> keyPoints;
     Mat descriptor;
     detector.detect(img, keyPoints, descriptor);
     extractor.compute(img, keyPoints, descriptor);
     matcher.knnMatch(descriptor, matches, 2);
+    NSLog(@"%.0f ms",[[NSDate date] timeIntervalSinceDate:date2] * 1000);
 //    int size = width * height;
 //    int result[size];
 //    memccpy(result, image, 0, size);  //    env->SetIntArrayRegion(result, 0, size, cbuf);
 //    free(image);                      //    env->ReleaseIntArrayElements(image, cbuf, 0);
 //    return result;//返回格式不对？？？
     
-    NSLog(@"%.0f ms",[[NSDate date] timeIntervalSinceDate:date] * 1000);
+//    NSLog(@"%.0f ms",[[NSDate date] timeIntervalSinceDate:date] * 1000);
 }
 
 - (void)didReceiveMemoryWarning
